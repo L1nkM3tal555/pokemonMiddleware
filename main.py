e6625aee00d0c3e46154384fdc8cfdda8c4add6d
Jesús Alejandro Dávila <69691593+L1nkM3tal555@users.noreply.github.com>
2024-10-05 11:47:04 -0500

    patch request done

--- main.py
+++ main.py
@@ -1,27 +1,57 @@
 import requests as req
 import uvicorn
 from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
+fakePokemonDb = {
+    25: {
+        "Nombre": "Pikachu",
+        "Habilidades": ["static", "lightning-rod"],
+        "Numero de la PokeDesk": 25,
+        "Tipo": ["electric"],
+        "Sprites": {"front_default": "https://example.com/sprite.png"}
+    }
+}
+
+# Definir el modelo que se espera para las actualizaciones
+class PokemonUpdate(BaseModel):
+    name: str | None = None
+    abilities: list[str] | None = None
+    sprites: dict | None = None
+    type: list[str] | None = None
+
+
 @app.middleware("http")
 async def processPokemons(request: Request, call_next):
+    try:
+        urlPath = request.url.path
+        if urlPath != '/' and urlPath != '/favicon.ico': 
+            if "pokemon" in urlPath:
+                id = urlPath.split('/')[-1]
 
-    urlPath = request.url.path
-
-    if "pokemon" in urlPath:
-        id = urlPath.split('/')[-1]
-
-        if (id.isdigit() == False) and (id.isalpha() == False):
-            raise HTTPException(status_code=400, detail="Identificador del pokemon no valido")
-    elif urlPath == "/favicon.ico":
-        print(urlPath)
-    else:
-        raise HTTPException(status_code=404, detail="URL no encontrada o no existe el endpoint")
+                if (id.isdigit() == False) and (id.isalpha() == False):
+                    raise HTTPException(status_code=400, detail="Identificador del pokemon no valido")
+            else:
+                raise HTTPException(status_code=404, detail="URL no encontrada o no existe el endpoint")
+        else:
+            print(urlPath)
+    except HTTPException as http_exc:
+        # Crear una respuesta con el código de estado y el mensaje de error
+        return JSONResponse(
+            status_code=http_exc.status_code,
+            content={"detail": http_exc.detail}
+        )
 
     response = await call_next(request)
     return response
+
+@app.get("/")
+def home():
+    return "API POKEDEX OAK // Autor: Jesús Alejandro Dávila Pinchao // Framework:FastAPI"
 
 @app.get("/api/general")
 def generalPokemons():
@@ -67,11 +97,49 @@ def getPokemon(id:str):
     for k in range(0, len(ab)):
         types.append(tp[k]["type"]["name"])
 
-    
     filterPokemon = {"Nombre": nombre, "Habilidades": skills, "Numero de la PokeDesk": pokeDesk,"Tipo":types, "Sprites": sp}
+    if pokeDesk not in fakePokemonDb:
+        fakePokemonDb[pokeDesk] = filterPokemon
     print(filterPokemon)
 
     return filterPokemon
+
+@app.patch("/api/pokemon/{pokeId}")
+async def update_pokemon(pokeId: int, pokemon: PokemonUpdate):
+
+    try:
+        id = pokeId
+        if id.isdigit() == False:
+            pokemons = req.get(url="https://pokeapi.co/api/v2/pokemon/", params=None).json()["results"]
+            for i in range(0, len(pokemons)):
+                pokemon = pokemons[i]
+                if pokemon["name"] == id:
+                    id = req.get(url=pokemon["url"], params=None).json()["id"]
+
+        if id not in fakePokemonDb:
+            raise HTTPException(status_code=404, detail="Pokemon no encontrado o no añadido, prueba hacer una petición GET del pokemon para que este sea añadido a la base de datos local")
+
+        # Obtener el Pokémon actual
+        storedPokemon = fakePokemonDb[id]
+        lastPokemon = storedPokemon
+        # Actualizar solo los campos proporcionados
+        
+        if pokemon.name is not None:
+            storedPokemon["Nombre"] = pokemon.name
+        if pokemon.abilities is not None:
+            storedPokemon["Habilidades"] = pokemon.abilities
+        if pokemon.sprites is not None:
+            storedPokemon["Sprites"] = pokemon.sprites
+        if pokemon.type is not None:
+            storedPokemon["Tipo"] = pokemon.type
+
+        fakePokemonDb[id] = storedPokemon
+        return {"message": "Pokemon updated", "last pokemon":lastPokemon, "pokemon": storedPokemon}
+    except HTTPException as http_exc:
+        return JSONResponse(
+            status_code=http_exc.status_code,
+            content={"detail": http_exc.detail}
+        )
 
 if __name__ == '__main__':
    uvicorn.run(app, host="localhost", port=8000)
